01e19113680ced8a5ef0aaab0dca90715c98b138
gab <guogabbi@gmail.com>
2022-01-17 14:14:46 -0800

    rendering Calendar weekdays using JS Date

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,21 +1,33 @@
 import React, { useEffect } from 'react';
 
 function Calendar({ inDate, outDate, changeDate }) {
-  let currentDate = new Date();
+  var currentDate = null;
   var displayedDate = null;
   var displayedYear = null;
   var displayedMonth = null;
+  var weekdays = getWeekdays();
   var daysInMonth = 0;
 
   useEffect(() => {
-    if (!displayedDate) {
-      displayedDate = currentDate;
-      displayedYear = displayedDate.getFullYear();
-      displayedMonth = displayedDate.getMonth();
-    }
+    currentDate = new Date();
+    displayedDate = currentDate;
+    displayedYear = displayedDate.getFullYear();
+    displayedMonth = displayedDate.getMonth();
+  }, [])
+
+  useEffect(() => {
     daysInMonth = getDaysInMonth(displayedYear, displayedMonth);
   }, [ displayedDate ]);
 
+  function getWeekdays() {
+    var weekdays = [];
+    var day = new Date(Date.UTC(2021, 0, 18));
+    for (let i = 0; i < 7; i++) {
+      weekdays.push(day.toLocaleDateString('en-US', { weekday: 'long' }).substring(0, 3));
+      day.setDate(day.getDate() + 1);
+    }
+    return weekdays;
+  }
   function getDaysInMonth(y, m) {
     return new Date(y, m, 0).getDate();
   }
@@ -33,31 +45,30 @@ function Calendar({ inDate, outDate, changeDate }) {
             <th colspan="7">Calendar</th>
           </tr>
           <tr>
-            <th>Su</th>
-            <th>M</th>
-            <th>Tu</th>
-            <th>W</th>
-            <th>Th</th>
-            <th>F</th>
-            <th>Sa</th>
+            {
+              weekdays.map(day => (<th>{day}</th>))
+            }
           </tr>
         </thead>
         <tbody>
           {
-            daysInMonth.map((date) => {
+            Array(daysInMonth).map((date) => {
+              // use negative number? same month, date: -weekday of 1st
               var CalendarElem = null;
 
-              if (displayedDate.getDay() == 6) {
-                return
-              } else if (displayedDate.getDay() == 0) {
-                return
-              }
-              // while the day does not match the day of the current col,
-              // create a day col in this row with no value
-              // increment the weekday
-              while(displayedDate.getDate() !== date) {
 
-              }
+
+              // if (displayedDate.getDay() == 6) {
+              //   return
+              // } else if (displayedDate.getDay() == 0) {
+              //   return
+              // }
+              // // while the day does not match the day of the current col,
+              // // create a day col in this row with no value
+              // // increment the weekday
+              // while(displayedDate.getDate() !== date) {
+
+              // }
               // create a day col with the date as the value
             })
           }
