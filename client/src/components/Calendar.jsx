7feef8a316f4c5cc5653e757c2dd883d27da3427
gab <guogabbi@gmail.com>
2022-01-17 20:38:26 -0800

    cleaned up calendar variables again, otherWeeks

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -2,23 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 function Calendar({ inDate, outDate, changeDate }) {
   var [ selectedDate, setSelectedDate ] = useState();
-  var [ displayedYear, setDisplayedYear ] = useState();
-  var [ displayedMonth, setDisplayedMonth ] = useState();
   var [ firstWeek, setFirstWeek ] = useState([]);
   var [ otherWeeks, setOtherWeeks ] = useState([]);
   var weekdays = getWeekdays();
 
   useEffect(() => {
-    new Promise(() => setSelectedDate(new Date()))
-    .then(() => setDisplayedYear(selectedDate.getFullYear()))
-    .then(() => setDisplayedMonth(selectedDate.getMonth()));
+    setSelectedDate(new Date());
   }, [])
 
   useEffect(() => {
     if (selectedDate) {
-      new Promise(() => setDisplayedYear(selectedDate.getFullYear()))
-      .then(() => setDisplayedMonth(selectedDate.getMonth()))
-      .then(() => setFirstWeek(getFirstWeek(displayedYear, displayedMonth)));
+      var y = selectedDate.getFullYear();
+      var m = selectedDate.getMonth();
+      setFirstWeek(getFirstWeek(y, m));
+      setOtherWeeks(getOtherWeeks(y, m, weekdays));
     }
   }, [ selectedDate ]);
 
@@ -43,14 +40,24 @@ function Calendar({ inDate, outDate, changeDate }) {
     return week;
   }
   function getOtherWeeks(y, m) {
-
+    var firstDay = new Date(y, m, 1).getDay();
+    var weeks = [];
+    var week = [];
+    for (let i = 8 - firstDay; i < new Date(y, m, 0).getDate(); i + 7) {
+      for (let j = 0; j < 7; j++) {
+        week.push(i + j);
+      }
+      weeks.push(week);
+    }
+    console.log(weeks);
+    return weeks;
   }
-  function clickNext() {
-    setSelectedDate(selectedDate.setMonth(displayedMonth + 1, 1));
-  }
-  function clickBack() {
-    setSelectedDate(selectedDate.setMonth(displayedMonth - 1, 1));
-  }
+  // function clickNext() {
+  //   setSelectedDate(selectedDate.setMonth(displayedMonth + 1, 1));
+  // }
+  // function clickBack() {
+  //   setSelectedDate(selectedDate.setMonth(displayedMonth - 1, 1));
+  // }
   return (
     <div className="calendar-group">
       {
@@ -58,19 +65,22 @@ function Calendar({ inDate, outDate, changeDate }) {
         <table className="calendar">
           <thead>
             <tr>
-              <th colspan="7">{ selectedDate.toLocaleDateString('en-US', { month: 'long' }) }</th>
+              <th colSpan="7">
+                { selectedDate.toLocaleDateString('en-US', { month: 'long' }) }
+              </th>
             </tr>
             <tr>
-              { weekdays.map(day => <th>{day}</th>) }
+              { weekdays.map((day, i) => <th key={i}>{day}</th>) }
             </tr>
           </thead>
           <tbody>
             <tr>
-              { firstWeek.map(date => <td>{date}</td>) }
+              { firstWeek.map((date, i) => <td key={i}>{date}</td>) }
             </tr>
-            { otherWeeks.map(date => <td>{date}</td>) }
+            {/* { otherWeeks.map((date, i) => <td>{date}</td>) } */}
           </tbody>
-        </table> : null
+        </table>
+        : null
       }
     </div>
   );
